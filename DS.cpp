b4aeca5d0d95d57a70fb15d3409274872ba4f09c
Seif Taher <seiftaher47@gmail.com>
2020-05-07 23:16:30 +0200

    DS V0.1

--- DS.cpp
+++ DS.cpp
@@ -1,8 +1,21 @@
-/*
- * DS Final Project.cpp
- *
- *  Created on: May 7, 2020
- *      Author: Seif
- */
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <queue>
+#include <filesystem>
+using namespace std;
+namespace fs = std::filesystem;
 
 
+int main()
+{
+	map<string,queue<int>> invertedIndex;
+
+
+
+    string path = "C:/Users/Seif/Desktop/Talta Comp/2nd Term/Data Structures/questions/questions";
+    for (const auto & entry : fs::directory_iterator(path))
+        cout << entry.path() << endl;
+
+}
