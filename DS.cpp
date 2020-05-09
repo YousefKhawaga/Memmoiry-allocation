bb73cb8a5f73d9bcd04dba6a8bd1671090441f4f
Yousef ElKhawaga <yousefkhawaga@hotmail.com>
2020-05-09 23:12:54 +0200

    DS

--- DS.cpp
+++ DS.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
-#include <map>
 #include <string>
 #include <fstream>
-#include <queue>
 #include <filesystem>
+#include "Trie.h"
+
+
 using namespace std;
 namespace fs = std::filesystem;
 
 
 int main()
 {
-	map<string,queue<int>> invertedIndex;
-
-
-
-    string path = "C:/Users/youse/OneDrive/Desktop/questions";
-    for (const auto & entry : fs::directory_iterator(path))
-        cout << entry.path() << endl;
+    Trie* head = new Trie();
+    string x;
+    ifstream inFile;
+    string path = "C:/Users/youse/OneDrive/Desktop/questions/0.txt";
+    inFile.open(path);
+    if (!inFile) {
+        cout << "Unable to open file";
+        exit(1); // terminate with error
+    }
+    while (inFile >> x) {
+        head->insert(x,path);
+    }
+    list<string> paths = head->search("What");
+    for (auto i = paths.begin(); i != paths.end(); i++)
+    {
+        cout << *i << endl;
+    }
+    //for (const auto& entry : fs::directory_iterator(path))
+       // cout << entry.path() << endl;
 
 }
